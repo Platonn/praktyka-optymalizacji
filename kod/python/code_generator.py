db4bba987c4b5aa3411825ea1d529919db356b32
Platonn <platonn@platonn.com>
2017-01-06 00:43:53 +0100

    Generowanie danych fix #5
    - generuja sie:
      - lista operacji (numery od 0 do o-1)
      - lista z kosztami Operacji
      - lista operacji wewnatrz roznych Zadan (podciagi listy operacji)
      - lista zadan przdzielonych do roznych (podciagi listy operacji)
      - wszystkie te dane laduja w obiekcie data i sa eksportowane do pliku (nazwa pliku = current timestamp)
      - poki co parametry zadania sa ustawione na sztywno:
        - n = m = 3
        - liczba operacji w kazdym Zadaniu = 4
        - liczba operacji przydzielonych do Kazdej maszyny = 4
        - (lacznie jest 12 zadan)

---
+++ kod/python/code_generator.py
@@ -0,0 +1,73 @@
+import numpy as np
+import random
+import time
+import datetime
+
+
+def genParameters():
+	n = m = 3  # for simplicity: same number of machines as tasks
+	o_per_n = o_per_m = 4  # for simplicity: same number of operations in task as in machine
+	o = n * o_per_n
+	maxCost = 100
+	return (o, n, o_per_n, m, o_per_m, maxCost)
+
+
+def genData((o, n, o_per_n, m, o_per_m, maxCost)):
+	operations = getOperations(o)
+	costs = genCosts(operations, maxCost)
+	tasks = genSubsequences(operations, n, o_per_n)
+	machines = genSubsequences(operations, m, o_per_m)
+	result = {
+		'operations': operations,
+		'costs':      costs,
+		'tasks':      tasks,
+		'machines':   machines
+	}
+	return result
+
+
+def genCosts(operations, maxCost):
+	return [random.randint(1, maxCost) for i in operations]
+
+
+def genSubsequences(operations, num, o_per_num):
+	tasks = []
+	for i in range(0, num):
+		subsequence = sorted(random.sample(operations, o_per_num))
+		tasks.append(subsequence)
+		operations = [i for i in operations if i not in subsequence]  # tasks - subsequence
+	return tasks
+
+
+def getOperations(o):
+	return np.arange(o)
+
+
+def exportToFile(data):
+	filename = genFilename()
+	np.save(filename, data)
+	return filename
+
+
+def importFromFile(filename):
+	return np.load(filename)
+
+
+def genFilename():
+	dir = 'data/'
+	ext = '.npy'
+	timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H-%M-%S')
+	result = dir + timestamp + ext
+	return result
+
+
+def main():
+	parameters = genParameters()
+	data = genData(parameters)
+
+	filename = exportToFile(data)
+	loadedData = importFromFile(filename)
+	print loadedData
+
+
+main()
